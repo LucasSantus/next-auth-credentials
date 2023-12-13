87514d1d4372cdb52d2ba537fa668c0099964bc6
Lucas Santos <leos.developer@gmail.com>
2023-12-13 16:05:47 -0300

    feat: update code of input

--- src/components/input-password.tsx
+++ src/components/input-password.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { EyeIcon, EyeOffIcon, Loader2Icon } from "lucide-react";
 import * as React from "react";
 import { Button } from "./ui/button";
 import { Input, InputProps } from "./ui/input";
@@ -8,7 +8,7 @@ import { Input, InputProps } from "./ui/input";
 export interface InputPasswordProps extends InputProps {}
 
 const InputPassword = React.forwardRef<HTMLInputElement, InputPasswordProps>(
-  ({ ...props }, ref) => {
+  ({ disabled, loading, ...rest }, ref) => {
     const [isShow, setIsShow] = React.useState<boolean>(false);
 
     function handleChangeType() {
@@ -17,15 +17,25 @@ const InputPassword = React.forwardRef<HTMLInputElement, InputPasswordProps>(
 
     return (
       <div className="relative">
-        <Input ref={ref} {...props} type={isShow ? "text" : "password"} />
+        <Input
+          ref={ref}
+          {...rest}
+          disabled={disabled}
+          type={isShow ? "text" : "password"}
+        />
         <Button
           variant="outline"
           size="icon"
           type="button"
           className="absolute bottom-0 right-0 top-0 z-10 rounded-bl-none rounded-tl-none"
           onClick={handleChangeType}
+          disabled={disabled}
         >
-          {isShow ? (
+          {loading ? (
+            <div className="flex h-full items-center justify-center">
+              <Loader2Icon className="h-5 w-5 animate-spin" />
+            </div>
+          ) : isShow ? (
             <EyeIcon className="h-4 w-4" />
           ) : (
             <EyeOffIcon className="h-4 w-4" />
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,35 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { Loader2Icon } from "lucide-react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  loading?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, loading, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="relative">
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {loading && (
+          <div className="absolute bottom-0 right-0 top-0 z-10 flex h-full w-10 items-center justify-center">
+            <Loader2Icon className="h-5 w-5 animate-spin" />
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
