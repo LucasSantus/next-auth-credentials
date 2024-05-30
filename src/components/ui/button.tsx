ad89f10d7437e1490968ea49af908aa06223673a
Lucas Santos <leos.developer@gmail.com>
2024-05-30 14:18:56 -0300

    :rocket: feat: add new components shadcn ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,84 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Loader2Icon } from "lucide-react";
+
+const buttonVariants = cva(
+  "gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-5 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  icon?: React.ReactNode;
+  isLoading?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading,
+      disabled,
+      icon,
+      children,
+      ...rest
+    },
+    ref,
+  ) => {
+    const Component = asChild ? Slot : "button";
+
+    const isDisabled = isLoading || disabled;
+
+    return (
+      <Component
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        disabled={isDisabled}
+        aria-disabled={isDisabled}
+        {...rest}
+      >
+        {isLoading ? (
+          <Loader2Icon className="mr-2 size-4 animate-spin" />
+        ) : (
+          icon
+        )}
+        {children}
+      </Component>
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
---
+++ src/components/ui/input.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Loader2Icon } from "lucide-react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  isLoading?: boolean;
+  startComponent?: React.ReactNode;
+  endComponent?: React.ReactNode;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    { className, type, isLoading, startComponent, endComponent, ...props },
+    ref,
+  ) => {
+    const componentClass =
+      "absolute bottom-0 top-0 z-10 flex h-full w-10 items-center justify-center";
+
+    return (
+      <div className="relative">
+        <input
+          type={type}
+          className={cn(
+            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+            { "pl-11": startComponent },
+            { "pr-11": endComponent },
+          )}
+          ref={ref}
+          {...props}
+        />
+
+        {!!startComponent && (
+          <div
+            className={cn(
+              "left-0 rounded-l-md border-y border-l border-input bg-background",
+              componentClass,
+            )}
+          >
+            {startComponent}
+          </div>
+        )}
+
+        {isLoading ? (
+          <div className={cn("right-0", componentClass)}>
+            <Loader2Icon className="h-5 w-5 animate-spin" />
+          </div>
+        ) : (
+          !!endComponent && (
+            <div
+              className={cn(
+                "right-0 rounded-r-md border-y border-r border-input bg-background",
+                componentClass,
+              )}
+            >
+              {endComponent}
+            </div>
+          )
+        )}
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
